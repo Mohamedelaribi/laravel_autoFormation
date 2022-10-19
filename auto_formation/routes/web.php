90d68500e5739d662f1ed301a7875b3981971d2a
Mohamedelaribi <mohamed.elaribi.solicode@gmail.com>
2022-10-19 13:56:27 +0100

    push

---
+++ auto_formation/routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CrudController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::get('/contact',function(){
+    return view('form');
+});
+
+Route::get('/index',[CrudController::class,'hello']);
+
+use Illuminate\Http\Request;
+Route::any('/form', function(Request $resquest){
+    $name = $resquest->test;
+    return $name;
+
+});
+
+
+
+Route::get('/index/{stor?}/{item?}',function($stor = NULL , $itme = null){
+    return $stor.' '. $itme;
+});
+
+
+Route::get('/prodect/{category?}/{itme?}', function($category = null, $itme = null){
+    if(isset($category)){
+        if(isset($itme)){
+            return "<h1>{$itme}</h1>";
+        }return "<h1>{$category}</h1>";
+    }return "<h1>stor</h1>";
+})->where(['category'=>'[A-Za-z]+','itme'=>'[A-Za-z]+']);
